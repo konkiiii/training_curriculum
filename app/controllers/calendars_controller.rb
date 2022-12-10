5a1027c4703fa7406156c2b4ecf3e96403186b41
konkiiii <kintaro0827@icloud.com>
2022-12-10 19:00:10 +0900

    Issue_3
    ビューを確認した際にハッシュの記述に間違いがあったので修正しています。

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -37,8 +37,9 @@ class CalendarsController < ApplicationController
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
       # days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans}
-      days = { month: '(@todays_date + x).month', date: '(@todays_date+x).day', plans: 'today_plans'}
+      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans }
       # ハッシュロケットからシンボル型に修正
+      # ハッシュの値はデータなので''で括らない
       @week_days.push(days)
     end
 
